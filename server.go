1bdfc5e32938be2fba637ca6ab0280a89ce4e933
Daniel <daniel_sousa.p@outlook.com>
2022-11-04 22:49:44 -0300

    feat: create server struct and helper func

---
+++ server.go
@@ -0,0 +1,13 @@
+package chat
+
+type server struct {
+	rooms    map[string]*room
+	commands chan command
+}
+
+func newServer() *server {
+	return &server{
+		rooms:    make(map[string]*room),
+		commands: make(chan command),
+	}
+}
