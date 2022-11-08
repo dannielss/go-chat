14022a1eb83174b36e278d95439205fbfb2be8a3
Daniel <daniel_sousa.p@outlook.com>
2022-11-07 23:37:51 -0300

    feat: create newClient func

--- server.go
+++ server.go
@@ -1,4 +1,9 @@
-package chat
+package main
+
+import (
+	"log"
+	"net"
+)
 
 type server struct {
 	rooms    map[string]*room
@@ -9,5 +14,15 @@ func newServer() *server {
 	return &server{
 		rooms:    make(map[string]*room),
 		commands: make(chan command),
+	}
+}
+
+func (s *server) newClient(conn net.Conn) {
+	log.Printf("new client has connected: %s", conn.RemoteAddr().String())
+
+	c := &client{
+		conn:     conn,
+		nick:     "anonymous",
+		commands: s.commands,
 	}
 }
